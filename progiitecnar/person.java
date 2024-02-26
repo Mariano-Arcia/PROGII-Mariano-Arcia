72a39683d1593b99c92e53a55e1b726953df621e
Mariano-Arcia <160240351+Mariano-Arcia@users.noreply.github.com>
2024-02-26 08:22:41 -0500

    Add files via upload

---
+++ progiitecnar/person.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.progiitecnar;
+
+/**
+ *
+ * @author Sala de Sistemas
+ */
+public class person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Personas p = new Personas();
+        p.setNombre("Mariano Arcia");
+        System.out.println(p.getNombre());
+    }
+    
+}
