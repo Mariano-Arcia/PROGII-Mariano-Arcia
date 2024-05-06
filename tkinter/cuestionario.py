5c6878f2d3148fff46c39747a159c601e7dd9767
Mariano-Arcia <160240351+Mariano-Arcia@users.noreply.github.com>
2024-05-06 12:56:40 -0500

    Cuestionario.py

--- tkinter/cuestionario.py
+++ tkinter/cuestionario.py
@@ -1,5 +1,5 @@
 import tkinter as tk 
-from PIL import  ImageTk 
+from PIL import ImageTk
 
 ventana = tk.Tk()
 
@@ -11,7 +11,7 @@ ventana.title("Ejercicio #2")
 ancho_pantalla = ventana.winfo_screenwidth() 
 alto_pantalla = ventana.winfo_screenheight() 
 
-ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}")
+ventana.geometry(f"{800}x{800}")
 ventana.config(bg="grey")
 
 def mostrar_datos():
@@ -19,54 +19,63 @@ def mostrar_datos():
     apellido = cd_2.get()
     edad = cd_3.get()
     direccion = cd_4.get()
-    telefono = cd_5.get
-    sexo = cd_6.get
-    ciudad = cd_7.get
-    datos_formateados = f"Nombre: {nombre}\nApellido: {apellido}\nEdad: {edad} \nDireccion: {direccion} \nTelefono: {telefono} \nSexo : {sexo} \nCiudad: {ciudad}"
+    telefono = cd_5.get()
+    sexo = var_genero.get()
+    ciudad = cd_7.get(cd_7.curselection())
+    datos_formateados = f"Nombre: {nombre}\nApellido: {apellido}\nEdad: {edad}\nDirección: {direccion}\nTeléfono: {telefono}\nGénero: {sexo}\nCiudad: {ciudad}"
     etiqueta_datos.config(text=datos_formateados)
 
-etiqueta = tk.Label(ventana, text="Hola", bg="grey", fg="white", font=("Arial", 40), width=40, height=2, anchor="center")
-etiqueta.grid(row=0, column=0, sticky="w", pady=2)
+etiqueta = tk.Label(ventana, text="Hola", bg="grey", fg="white", font=("Arial", 40))
+etiqueta.grid(row=0, column=0, columnspan=2, pady=10)
 
-cd_1 = tk.Entry(ventana, width=30, font=("Arial",15))
-cd_1.grid(row=1, column=0, sticky="w", pady=2)
+cd_1 = tk.Entry(ventana, width=10, font=("Arial",15))
+cd_1.grid(row=1, column=1, sticky="w", pady=2)
 bd_1 = tk.Button(ventana, text="Ingresar nombre", command=mostrar_datos, font=("Arial",15))
 bd_1.grid(row=1, column=0, sticky="w", pady=2)
 
-cd_2 = tk.Entry(ventana, width=30, font=("Arial",15))
-cd_2.grid(row=2, column=0, sticky="w", pady=2)
+cd_2 = tk.Entry(ventana, width=10, font=("Arial",15))
+cd_2.grid(row=2, column=1, sticky="w", pady=2)
 bd_2 = tk.Button(ventana, text="Ingresar apellido", command=mostrar_datos, font=("Arial",15))
 bd_2.grid(row=2, column=0, sticky="w", pady=2)
 
-cd_3 = tk.Entry(ventana, width=30, font=("Arial",15))
-cd_3.grid(row=3, column=0, sticky="w", pady=2)
+cd_3 = tk.Entry(ventana, width=10, font=("Arial",15))
+cd_3.grid(row=3, column=1, sticky="w", pady=2)
 bd_3 = tk.Button(ventana, text="Ingresar edad", command=mostrar_datos, font=("Arial",15))
 bd_3.grid(row=3, column=0, sticky="w", pady=2)
 
-cd_4 = tk.Entry(ventana, width=30, font=("Arial",15))
-cd_4.grid(row=4, column=0, sticky="w", pady=2)
-bd_4 = tk.Button(ventana, text="Ingresar direccion", command=mostrar_datos, font=("Arial",15))
+cd_4 = tk.Entry(ventana, width=10, font=("Arial",15))
+cd_4.grid(row=4, column=1, sticky="w", pady=2)
+bd_4 = tk.Button(ventana, text="Ingresar dirección", command=mostrar_datos, font=("Arial",15))
 bd_4.grid(row=4, column=0, sticky="w", pady=2)
 
-
-cd_5 = tk.Entry(ventana, width=30, font=("Arial",15))
-cd_5.grid(row=5, column=0, sticky="w", pady=2)
-bd_5 = tk.Button(ventana, text="Ingresar telefono", command=mostrar_datos, font=("Arial",15))
+cd_5 = tk.Entry(ventana, width=10, font=("Arial",15))
+cd_5.grid(row=5, column=1, sticky="w", pady=2)
+bd_5 = tk.Button(ventana, text="Ingresar teléfono", command=mostrar_datos, font=("Arial",15))
 bd_5.grid(row=5, column=0, sticky="w", pady=2)
 
-
-cd_6 = tk.Entry(ventana, width=30, font=("Arial",15))
+cd_6 = tk.Label(ventana,text="Género", font=("Arial",15))
 cd_6.grid(row=6, column=0, sticky="w", pady=2)
-bd_6 = tk.Button(ventana, text="Ingresar sexo", command=mostrar_datos, font=("Arial",15))
-bd_6.grid(row=6, column=0, sticky="w", pady=2)
 
+var_genero = tk.StringVar()
+var_genero.set("Masculino")
 
-cd_7 = tk.Entry(ventana, width=30, font=("Arial",15))
-cd_7.grid(row=7, column=0, sticky="w", pady=2)
-bd_7 = tk.Button(ventana, text="Ingresar ciudad", command=mostrar_datos, font=("Arial",15))
-bd_7.grid(row=7, column=0, sticky="w", pady=2)
+rb_hombre = tk.Radiobutton(ventana, text="Masculino", variable=var_genero, value="Masculino")
+rb_mujer = tk.Radiobutton(ventana, text="Femenino", variable=var_genero, value="Femenino")
+rb_hombre.grid(row=7, column=0, sticky="w", pady=2)
+rb_mujer.grid(row=8, column=0, sticky="w", pady=2)
 
-etiqueta_datos = tk.Label(ventana, text="", bg="grey", fg="white", font=("Arial", 20), width=50, height=4, anchor="center")
-etiqueta_datos.grid(row=5, column=0, sticky="w", pady=2)
+bd_ciudad = tk.Button(ventana, text="Seleccionar ciudad", command=mostrar_datos, font=("Arial",15))
+bd_ciudad.grid(row=9, column=0, pady=10)
+
+cd_7 = tk.Listbox(ventana, width=10, selectmode="single", font=("Arial",15))
+ciudades = ["Bogota", "Cartagena", "Barranquilla", "Cali", "Medellin"]
+
+for ciudad in ciudades:
+    cd_7.insert(tk.END, ciudad)
+
+cd_7.grid(row=10, column=0, sticky="w", pady=2)
+
+etiqueta_datos = tk.Label(ventana, text="", bg="grey", fg="white", font=("Arial", 20), width=10, height=4, anchor="center")
+etiqueta_datos.grid(row=11, column=0, sticky="w", pady=10)
 
 ventana.mainloop()
